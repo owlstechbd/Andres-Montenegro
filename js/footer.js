b2ccb0285305fa24a1ed36cc0f05d952953580e4
owlstechbd <owlstech.net@gmail.com>
2025-07-01 23:13:05 +0600

    first commit

---
+++ js/footer.js
@@ -0,0 +1,20 @@
+const mainfooter = `
+           <section id="main-footer">
+        <div class="container footer-main">
+
+            <div class="footer-left">
+                <p>© 2025 Andres Montenegro All Rights Reserved.</p>
+            </div>
+
+            <div class="footer-right">
+                <ul>
+                    <li><a href="mailto:andresmontenegro@ucwv.edu" target="_blank"><img src="images/email.png" alt=""></a></li>
+                    <li><a href="https://www.linkedin.com/in/andres-montenegro-727636256/" target="_blank"><img src="images/linkedin.png" alt=""></a></li>
+                </ul>
+            </div>
+
+        </div>
+    </section>
+
+`
+document.getElementById('main-footer').innerHTML = mainfooter
---
+++ js/menu.js
@@ -0,0 +1,60 @@
+const mainMenu = `
+        <section id="navbar">
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <a class="navbar-brand" href="index.html"> Andres Montenegro  </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav">
+
+                        <li class="nav-item">
+                            <a href="index.html">Home</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="about.html">About</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="profile.html">Profile</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="artifact.html"> Artifacts</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="essay.html"> Essays</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="leadership.html"> Leadership</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="research.html"> Research</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="#"> Repository</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="contact.html"> Contact</a>
+                        </li>
+
+                    </ul>
+
+                </div>
+
+
+            </div>
+        </nav>
+    </section>
+
+`
+document.getElementById('main-menu').innerHTML = mainMenu
